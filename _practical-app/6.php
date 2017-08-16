9a54df0dd1790076123430cc90e00e5abbb47050
AhmedShab <ahmed.vuw@gmail.com>
2017-08-16 14:48:33 +1200

    finished php6

--- _practical-app/6.php
+++ _practical-app/6.php
@@ -5,24 +5,32 @@
 	<section class="content">
 
 		<aside class="col-xs-4">
-		
+
 		<?php Navigation();?>
-			
+
 		</aside><!--SIDEBAR-->
 
 
 <article class="main-content col-xs-8">
- 
 
-	<?php  
+
+	<?php
 
 /*  Step1: Make a form that submits one value to POST super global
 
 
  */
 
-	
+ if ($_POST) {
+	 print_r($_POST);
+	 echo "<br>";
+	 echo "your name that was submitted into the form was {$_POST["name"]}";
+ }
 ?>
+
+<form class="" action="6.php" method="post">
+	Name: <input type="text" name="name" value="">
+</form>
 
 
 </article><!--MAIN CONTENT-->
